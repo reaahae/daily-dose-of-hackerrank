54978f2a20469677957a0d2e6ea70400cc7149d1
reaahae <82507473+reaahae@users.noreply.github.com>
2025-04-14 00:02:36 +0530

    Update 💉cure.py

--- 💊 Dose 01 - 1 Month Interview Prep Kit/🦠 Illness 01 - Problem Solving - Basic/👤 Patient 07_Diagonal_Difference/💉cure.py
+++ 💊 Dose 01 - 1 Month Interview Prep Kit/🦠 Illness 01 - Problem Solving - Basic/👤 Patient 07_Diagonal_Difference/💉cure.py
@@ -6,6 +6,7 @@ import random
 import re
 import sys
 
+#Solution 1
 # Function to compute the absolute difference between the sums of the diagonals of a square matrix
 def diagonalDifference(arr):
     diag1 = 0  # To store the sum of the primary diagonal (top-left to bottom-right)
@@ -13,11 +14,13 @@ def diagonalDifference(arr):
     index = len(arr)  # Number of rows/columns in the square matrix
 
     # Loop through each row of the matrix
-    for i in range(len(arr)):
-        # Primary diagonal element is where row index == column index
-        diag1 += arr[i][i]
-        
-        # Decrement the index to get the corresponding column for secondary diagonal
+   for i in range(len(arr)):
+        for j in range(len(arr)):
+            # Primary diagonal element is where row index == column index
+            if (i == j):
+                diag1 += arr[i][j]
+
+         # Decrement the index to get the corresponding column for secondary diagonal
         index = index - 1
         diag2 += arr[i][index]
 
@@ -26,7 +29,18 @@ def diagonalDifference(arr):
 
     return result
 
-# Entry point of the script
+#Solution 2
+def diagonalDifference(arr):
+    diag1 = 0  # Sum of primary diagonal
+    diag2 = 0  # Sum of secondary diagonal
+    n = len(arr)
+
+    for i in range(n):
+        diag1 += arr[i][i]           # Primary diagonal element
+        diag2 += arr[i][n - 1 - i]   # Secondary diagonal element
+
+    return abs(diag1 - diag2)
+
 if __name__ == '__main__':
     # Open a file to write the output (environment variable OUTPUT_PATH must be set)
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
